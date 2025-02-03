140694c95864b8418f1a5bb8237a1b21f4a474e0
Lucas <lucasnrgaard@gmail.com>
2025-02-03 04:24:01 +0100

    dump

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
 import react from '@astrojs/react';
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://mojis.dev",
+  integrations: [
+    react(),
+    sitemap()
+  ],
   vite: {
     plugins: [
       tailwindcss()
